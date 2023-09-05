29c6a62926ab49f53252e60a4ba50da7c0dbcebb
viktorbudnik <viktorbudnik1@gmail.com>
2023-09-05 16:37:43 +0300

    Servlet

--- src/main/java/RedirectServlet.java
+++ src/main/java/RedirectServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class RedirectServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect(req.getContextPath() + "/greeting");
+        resp.sendRedirect(req.getContextPath() + "/greeting?name=Viktor");
 
     }
 }
